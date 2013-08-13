41df9206c4218c46aa176e519c7f2a90f9f22797
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2013-08-13 18:21:12 +0200

    add probably final Attribute<bool>

--- include/propertyzeug/Attribute.h
+++ include/propertyzeug/Attribute.h
@@ -3,11 +3,37 @@
 
 #include <propertyzeug/propertyzeug.h>
 #include "BaseAttribute.h"
+#include "LimitAttribute.h"
 
 namespace propertyzeug {
 
 template <typename Type>
 class Attribute;
+
+template <>
+class Attribute<bool> : public BaseAttribute<bool>
+{
+public:
+    Attribute(const std::string & name, const std::string & title, const bool & value)
+    :   BaseAttribute<bool>(name, title, value) {};
+
+    Attribute(const std::string & name, const std::string & title, 
+                  const std::function<const bool & ()> & getter,
+                  const std::function<void(const bool &)> & setter)
+    :   BaseAttribute<bool>(name, title, getter, setter) {};
+    
+    template <class Object>
+    Attribute(const std::string & name, const std::string & title,
+                  Object & object, const bool & (Object::*getter_pointer)() const,
+                  void (Object::*setter_pointer)(const bool &))
+    :   BaseAttribute<bool>(name, title, object, getter_pointer, setter_pointer) {};
+    
+    template <class Object>
+    Attribute(const std::string & name, const std::string & title,
+                  Object & object, bool (Object::*getter_pointer)() const,
+                  void (Object::*setter_pointer)(bool))
+    :   BaseAttribute<bool>(name, title, object, getter_pointer, setter_pointer) {};
+};
 
 template <>
 class Attribute<int> : public BaseAttribute<int>
