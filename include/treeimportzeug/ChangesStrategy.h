1a7a2f38a0402dafb43702e8a783fc5de5d12974
Willy Scheibel <willyscheibel@gmx.de>
2013-08-13 18:05:18 +0200

    work on Change format import sqlite strategy

--- include/treeimportzeug/ChangesStrategy.h
+++ include/treeimportzeug/ChangesStrategy.h
@@ -2,6 +2,10 @@
 
 #include <TreeSqliteParserStrategy.h>
 
+#include <Tree.h>
+
+#include <QList>
+
 class ChangesStrategy : public TreeSqliteParserStrategy
 {
 public:
@@ -9,4 +13,13 @@ public:
 	
 	virtual void processOne();
 	virtual void processMultiple();
+protected:
+	QList<Tree*> _trees;
+	mutable QHash<long, int> _ids;
+	mutable int _nextId;
+	
+	void createTreeForRevision(unsigned revisionId);
+	void transferTrees();
+	int idFor(long hash) const;
+	void insertIntoTree(Node* node, Tree* tree, const QHash<int, Node*>& nodes, const QHash<int, int>& parentIds) const;
 };
--- source/treeimportzeug/ChangesStrategy.cpp
+++ source/treeimportzeug/ChangesStrategy.cpp
@@ -3,21 +3,99 @@
 #include <TreeSqliteParser.h>
 
 #include <QHash>
-#include <QVariantMap>
-
 #include <QDebug>
 
 ChangesStrategy::ChangesStrategy(TreeSqliteParser& parser)
 : TreeSqliteParserStrategy(parser)
+, _nextId(0)
 {
 }
 
 void ChangesStrategy::processOne()
 {
+	for (const QVariantMap& revision : executeQuery("SELECT id FROM revisions WHERE 1 ORDER BY id LIMIT 1"))
+	{
+		createTreeForRevision(revision["id"].toUInt());
+	}
 	
+	transferTrees();
 }
 
 void ChangesStrategy::processMultiple()
 {
+	for (const QVariantMap& revision : executeQuery("SELECT id FROM revisions WHERE 1 ORDER BY id"))
+	{
+		createTreeForRevision(revision["id"].toUInt());
+	}
 	
+	transferTrees();
+}
+
+void ChangesStrategy::createTreeForRevision(unsigned revisionId)
+{
+	Tree* tree = new Tree();
+	
+	_trees << tree;
+	
+	QHash<int, Node*> nodes;
+	QHash<int, int> parentIds;
+	QHash<int, long> hashes;
+	
+	QList<QVariantMap> rows = executeQuery("SELECT id, hash, type_id, parent_id FROM nodes WHERE revision_id = " + QString::number(revisionId) + " ORDER BY id");
+	for (const QVariantMap& node : rows)
+	{
+		int id = idFor(node["hash"].toInt());
+		nodes[id] = new Node(id);
+		
+		hashes[node["id"].toInt()] = node["hash"].toInt();
+	}
+	
+	for (const QVariantMap& node : rows)
+	{
+		parentIds[idFor(node["hash"].toInt())] = idFor(hashes[node["parent_id"].toInt()]);
+	}
+	
+	for (Node* node : nodes)
+	{
+		insertIntoTree(node, tree, nodes, parentIds);
+	}
+}
+
+void ChangesStrategy::insertIntoTree(Node* node, Tree* tree, const QHash<int, Node*>& nodes, const QHash<int, int>& parentIds) const
+{
+	if (tree->getNode(node->id()))
+	{
+		return;
+	}
+	
+	if (parentIds.value(node->id(), 0) <= 0)
+	{
+		tree->root()->addChild(node);
+		
+		return;
+	}
+	
+	if (!tree->getNode(parentIds.value(node->id(), 0)))
+	{
+		insertIntoTree(nodes.value(parentIds.value(node->id(), 0), nullptr), tree, nodes, parentIds);
+	}
+	
+	tree->getNode(parentIds.value(node->id(), 0))->addChild(node);
+}
+
+void ChangesStrategy::transferTrees()
+{
+	_parser.trees() = _trees;
+}
+
+int ChangesStrategy::idFor(long hash) const
+{
+	if (!_ids.contains(hash))
+	{
+		_ids[hash] = _nextId;
+		
+		++_nextId;
+	}
+	
+	return _ids[hash];
 }
