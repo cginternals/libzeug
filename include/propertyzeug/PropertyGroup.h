10e0754eb52f2b7815f7bd8215a6ebc6a73e1d46
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2013-08-19 16:09:52 +0200

    implement declared PropertyGroup interface

--- include/propertyzeug/PropertyGroup.h
+++ include/propertyzeug/PropertyGroup.h
@@ -1,7 +1,8 @@
 
 #pragma once
 
-#include <forward_list>
+#include <assert.h>
+#include <list>
 #include <unordered_map>
 #include <propertyzeug/propertyzeug.h>
 #include "AbstractProperty.h"
@@ -39,16 +40,20 @@ public:
     
 
     AbstractProperty & property(const std::string & name);
+    const AbstractProperty & property(const std::string & name) const;
 
     template <typename Type>
     Property<Type> & property(const std::string & name);
+    
+    template <typename Type>
+    const Property<Type> & property(const std::string & name) const;
     
 
     template <typename Type>
     const Type & value(const std::string & name) const;
     
     template <typename Type>
-    bool setValue(const std::string & name, const Type & value);
+    void setValue(const std::string & name, const Type & value);
 
     
     bool propertyExists(const std::string & name) const;
@@ -63,10 +68,64 @@ public:
     
     AbstractProperty * obtainProperty(const std::string & name);
     
-    
 protected:
-    std::forward_list<AbstractProperty *> m_properties;
+    std::list<AbstractProperty *> m_properties;
     std::unordered_map<std::string, AbstractProperty *> m_propertiesMap;
 };
+
+template <typename Type>
+bool PropertyGroup::addProperty(const std::string & name,
+    const std::string & title, const Type & value)
+{
+    return this->addProperty(new Property<Type>(name, title, value));
+}
+
+template <typename Type>
+bool PropertyGroup::addProperty(const std::string & name, const std::string & title,
+    const std::function<const Type & ()> & getter,
+    const std::function<void(const Type &)> & setter)
+{
+    return this->addProperty(new Property<Type>(name, title, getter, setter));
+}
+
+template <typename Type, class Object>
+bool PropertyGroup::addProperty(const std::string & name, const std::string & title,
+    Object & object, const Type & (Object::*getter_pointer)() const,
+    void (Object::*setter_pointer)(const Type &))
+{
+    return this->addProperty(new Property<Type>(name, title, object, getter_pointer, setter_pointer));
+}
+
+template <typename Type, class Object>
+bool PropertyGroup::addProperty(const std::string & name, const std::string & title,
+    Object & object, Type (Object::*getter_pointer)() const,
+    void (Object::*setter_pointer)(Type))
+{
+    return this->addProperty(new Property<Type>(name, title, object, getter_pointer, setter_pointer));
+}
+
+template <typename Type>
+Property<Type> & PropertyGroup::property(const std::string & name)
+{
+    return *(this->property(name).to<Property<Type>>());
+}
+    
+template <typename Type>
+const Property<Type> & PropertyGroup::property(const std::string & name) const
+{
+    return *(this->property(name).to<Property<Type>>());
+}
+
+template <typename Type>
+const Type & PropertyGroup::value(const std::string & name) const
+{
+    return this->property(name).to<Property<Type>>()->value();
+}
+
+template <typename Type>
+void PropertyGroup::setValue(const std::string & name, const Type & value)
+{
+    this->property(name).to<Property<Type>>()->setValue(value);
+}
 
 } // namespace
--- source/propertyzeug/PropertyGroup.cpp
+++ source/propertyzeug/PropertyGroup.cpp
@@ -3,6 +3,87 @@
 
 namespace propertyzeug {
     
+PropertyGroup::PropertyGroup(std::string name, std::string title)
+:   AbstractProperty(name, title)
+{
+}
 
+PropertyGroup::~PropertyGroup()
+{
+    // TODO delete all properties?
+}
+
+bool PropertyGroup::addProperty(AbstractProperty * property)
+{
+    if (this->propertyExists(property->name()))
+        return false;
+    
+    m_properties.push_back(property);
+    m_propertiesMap.insert(std::make_pair(property->name(), property));
+    return true;
+}
+
+AbstractProperty & PropertyGroup::property(const std::string & name)
+{
+    assert(this->propertyExists(name));
+    return *m_propertiesMap.at(name);
+}
+    
+const AbstractProperty & PropertyGroup::property(const std::string & name) const
+{
+    assert(this->propertyExists(name));
+    return *m_propertiesMap.at(name);
+}
+
+bool PropertyGroup::propertyExists(const std::string & name) const
+{
+    if (m_propertiesMap.find(name) == m_propertiesMap.end())
+        return false;
+    else
+        return true;
+}
+
+AbstractProperty * PropertyGroup::replaceProperty(const std::string & name, AbstractProperty * property)
+{
+    if (!this->propertyExists(name))
+        return nullptr;
+    
+    this->insertPropertyAfter(name, property);
+    return this->obtainProperty(name);
+}
+
+bool PropertyGroup::insertPropertyAfter(const std::string & name, AbstractProperty * property)
+{
+    if (!this->propertyExists(name) || this->propertyExists(property->name()))
+        return false;
+    
+    auto propertyIterator = m_properties.begin();
+    while (name == (*(propertyIterator++))->name());
+    
+    m_properties.insert(propertyIterator, property);
+    m_propertiesMap.insert(std::make_pair(property->name(), property));
+    return true;
+}
+
+bool PropertyGroup::removeProperty(AbstractProperty * property)
+{
+    if (!this->propertyExists(property->name()))
+        return false;
+    
+    m_properties.remove(property);
+    m_propertiesMap.erase(property->name());
+    return true;
+}
+
+AbstractProperty * PropertyGroup::obtainProperty(const std::string & name)
+{
+    if (!this->propertyExists(name))
+        return nullptr;
+    
+    AbstractProperty * property = m_propertiesMap.find(name)->second;
+    m_properties.remove(property);
+    m_propertiesMap.erase(property->name());
+    return property;
+}
     
 } // namespace
