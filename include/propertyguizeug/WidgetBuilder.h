ec7a167cc5d3ba37827728e0594e98e9bc407df1
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2013-09-02 10:37:44 +0200

    add propertyguizeug lib

---
+++ include/propertyguizeug/WidgetBuilder.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include <propertyzeug/AbstractPropertyVisitor.h>
+#include "propertyguizeug.h"
+
+namespace propertyguizeug {
+    
+class PROPERTYGUIZEUG_API WidgetBuilder
+{
+
+};
+
+} // namespace
