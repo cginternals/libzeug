66e9a6cf7211744890ed1daa96e833522cad2ca0
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2013-09-11 13:43:46 +0200

    rename typename Array to Vector

--- include/propertyzeug/VectorProperty.h
+++ include/propertyzeug/VectorProperty.h
@@ -15,30 +15,30 @@ namespace propertyzeug {
  * an empty container to the constructor.
  */
     
-template <typename Array>
-class VectorProperty : public BaseProperty<Array>
+template <typename Vector>
+class VectorProperty : public BaseProperty<Vector>
 {
 public:
-    VectorProperty(const std::string & name, const Array & value);
+    VectorProperty(const std::string & name, const Vector & value);
     
     VectorProperty(const std::string & name,
-                  const std::function<const Array & ()> & getter,
-                  const std::function<void(const Array &)> & setter);
-    
-    template <class Object>
-    VectorProperty(const std::string & name,
-                  Object & object, const Array & (Object::*getter_pointer)() const,
-                  void (Object::*setter_pointer)(const Array &));
+                  const std::function<const Vector & ()> & getter,
+                  const std::function<void(const Vector &)> & setter);
     
     template <class Object>
     VectorProperty(const std::string & name,
-                  Object & object, Array (Object::*getter_pointer)() const,
-                  void (Object::*setter_pointer)(Array));
+                  Object & object, const Vector & (Object::*getter_pointer)() const,
+                  void (Object::*setter_pointer)(const Vector &));
+    
+    template <class Object>
+    VectorProperty(const std::string & name,
+                  Object & object, Vector (Object::*getter_pointer)() const,
+                  void (Object::*setter_pointer)(Vector));
 
     virtual ~VectorProperty();
     
-    virtual const Array & value() const;
-    virtual void setValue(const Array & value);
+    virtual const Vector & value() const;
+    virtual void setValue(const Vector & value);
     
     unsigned int fixedSize() const;
 
@@ -53,84 +53,84 @@ protected:
     
 };
 
-template <typename Array>
-VectorProperty<Array>::VectorProperty(const std::string & name,
-    const Array & value)
-:   BaseProperty<Array>(name, value)
+template <typename Vector>
+VectorProperty<Vector>::VectorProperty(const std::string & name,
+    const Vector & value)
+:   BaseProperty<Vector>(name, value)
 ,   m_fixedSize(this->m_value->get().size())
 ,   m_dimensions({m_fixedSize})
 {
     assert(m_fixedSize != 0);
 }
 
-template <typename Array>
-VectorProperty<Array>::VectorProperty(const std::string & name,
-    const std::function<const Array & ()> & getter,
-    const std::function<void(const Array &)> & setter)
-:   BaseProperty<Array>(name, getter, setter)
+template <typename Vector>
+VectorProperty<Vector>::VectorProperty(const std::string & name,
+    const std::function<const Vector & ()> & getter,
+    const std::function<void(const Vector &)> & setter)
+:   BaseProperty<Vector>(name, getter, setter)
 ,   m_fixedSize(this->m_value->get().size())
 ,   m_dimensions({m_fixedSize})
 {
     assert(m_fixedSize != 0);
 }
 
-template <typename Array>
+template <typename Vector>
 template <class Object>
-VectorProperty<Array>::VectorProperty(const std::string & name,
-    Object & object, const Array & (Object::*getter_pointer)() const,
-    void (Object::*setter_pointer)(const Array &))
-:   BaseProperty<Array>(name, object, getter_pointer, setter_pointer)
+VectorProperty<Vector>::VectorProperty(const std::string & name,
+    Object & object, const Vector & (Object::*getter_pointer)() const,
+    void (Object::*setter_pointer)(const Vector &))
+:   BaseProperty<Vector>(name, object, getter_pointer, setter_pointer)
 ,   m_fixedSize(this->m_value->get().size())
 ,   m_dimensions({m_fixedSize})
 {
     assert(m_fixedSize != 0);
 }
 
-template <typename Array>
+template <typename Vector>
 template <class Object>
-VectorProperty<Array>::VectorProperty(const std::string & name,
-    Object & object, Array (Object::*getter_pointer)() const,
-    void (Object::*setter_pointer)(Array))
-:   BaseProperty<Array>(name, object, getter_pointer, setter_pointer)
+VectorProperty<Vector>::VectorProperty(const std::string & name,
+    Object & object, Vector (Object::*getter_pointer)() const,
+    void (Object::*setter_pointer)(Vector))
+:   BaseProperty<Vector>(name, object, getter_pointer, setter_pointer)
 ,   m_fixedSize(this->m_value->get().size())
 ,   m_dimensions({m_fixedSize})
 {
     assert(m_fixedSize != 0);
 }
 
-template <typename Array>
-VectorProperty<Array>::~VectorProperty()
+template <typename Vector>
+VectorProperty<Vector>::~VectorProperty()
 {
 }
 
-template <typename Array>
-const Array & VectorProperty<Array>::value() const
+template <typename Vector>
+const Vector & VectorProperty<Vector>::value() const
 {
     assert(this->m_value->get().size() == m_fixedSize);
-    return BaseProperty<Array>::value();
+    return BaseProperty<Vector>::value();
 }
 
-template <typename Array>
-void VectorProperty<Array>::setValue(const Array & value)
+template <typename Vector>
+void VectorProperty<Vector>::setValue(const Vector & value)
 {
     assert(value.size() == m_fixedSize);
-    BaseProperty<Array>::setValue(value);
+    BaseProperty<Vector>::setValue(value);
 }
     
-template <typename Array>
-unsigned int VectorProperty<Array>::fixedSize() const
+template <typename Vector>
+unsigned int VectorProperty<Vector>::fixedSize() const
 {
     return m_fixedSize;
 }
 
-template <typename Array>
-const std::vector<unsigned int> & VectorProperty<Array>::dimensions() const
+template <typename Vector>
+const std::vector<unsigned int> & VectorProperty<Vector>::dimensions() const
 {
     return m_dimensions;
 }
 
-template <typename Array>
-void VectorProperty<Array>::setDimensions(const std::vector<unsigned int> & dimensions)
+template <typename Vector>
+void VectorProperty<Vector>::setDimensions(const std::vector<unsigned int> & dimensions)
 {    
     assert(m_fixedSize == std::accumulate(dimensions.begin(), dimensions.end(), 1,
                                           [](unsigned int a, unsigned int b) {
@@ -140,8 +140,8 @@ void VectorProperty<Array>::setDimensions(const std::vector<unsigned int> & dime
     m_dimensions = dimensions;
 }
 
-template <typename Array>
-std::string VectorProperty<Array>::valueAsString() const
+template <typename Vector>
+std::string VectorProperty<Vector>::valueAsString() const
 {
     std::stringstream stream;
     stream << "(";
