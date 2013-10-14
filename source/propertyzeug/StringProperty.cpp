49961750bc80c29db29e28e671565422c9dd23f7
Max Jendruk <max.jendruk@googlemail.com>
2013-10-15 00:51:24 +0200

    remove old file and comments

--- source/propertyzeug/StringProperty.cpp
+++ source/propertyzeug/StringProperty.cpp
@@ -45,19 +45,16 @@ const std::vector<std::string> & StringProperty::choices() const
 void StringProperty::clearChoices()
 {
     m_choices.clear();
-    // this->m_announcer.notify(events::kChoicesChanged);
 }
  
 void StringProperty::setChoices(const std::vector<std::string> & choices)
 {
     m_choices = choices;
-    // this->m_announcer.notify(events::kChoicesChanged);
 }
  
 void StringProperty::addChoice(const std::string & string)
 {
     m_choices.push_back(string);
-    // this->m_announcer.notify(events::kChoicesChanged);
 }
     
 std::string StringProperty::valueAsString() const
