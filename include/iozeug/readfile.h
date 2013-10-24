ece5e90c776e51906a050c0afc7525844821efa2
Willy Scheibel <willyscheibel@gmx.de>
2013-10-24 11:15:18 +0200

    Add threadingzeug and iozeug

---
+++ include/iozeug/readfile.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+#include <iozeug/iozeug.h>
+
+namespace zeug {
+
+// copied from glow::internal::FileReader
+IOZEUG_API bool readFile(const std::string& filePath, std::string& content);
+IOZEUG_API std::string readFile(const std::string& filePath);
+
+} // namespace zeug
---
+++ source/iozeug/readfile.cpp
@@ -0,0 +1,33 @@
+#include <iozeug/readfile.h>
+
+#include <fstream>
+
+namespace zeug {
+
+bool readFile(const std::string& filePath, std::string& content)
+{
+	// http://insanecoding.blogspot.de/2011/11/how-to-read-in-file-in-c.html
+
+	std::ifstream in(filePath, std::ios::in | std::ios::binary);
+
+	if (!in)
+	{
+		return false;
+	}
+
+	content = std::string(std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>());
+
+	return true;
+}
+
+// convenience
+std::string readFile(const std::string& filePath)
+{
+	std::string content;
+
+	readFile(filePath, content);
+
+	return content;
+}
+
+} // namespace zeug
