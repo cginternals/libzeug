cf3a29399ab2c250db10ec7978fb447db35196c2
Stefan Buschmann <stefan.buschmann@hpi.uni-potsdam.de>
2013-12-10 16:45:08 +0100

    Experiment with variadic templates

--- examples/script/main.cpp
+++ examples/script/main.cpp
@@ -8,8 +8,9 @@ using namespace scriptzeug;
 int main(int argc, char const *argv[])
 {
     ScriptEnvironment scripting;
-    scripting.evaluate("var a=2; a*2;");
-    scripting.evaluate("a*3;");
+//    scripting.evaluate("var a=2; a*2;");
+//    scripting.evaluate("a*3;");
+    scripting.evaluate("test(1, 2, '3', 23.42, 'asd');");
 
     return 0;
 }
--- source/scriptzeug/BackendJavaScript/JSScriptEnvironment.cpp
+++ source/scriptzeug/BackendJavaScript/JSScriptEnvironment.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <scriptzeug/BackendJavaScript/JSScriptEnvironment.h>
 
 
@@ -8,14 +9,109 @@ namespace scriptzeug
 using namespace v8;
 
 
+template <typename... Arguments>
+class ArgParser
+{
+public:
+    ArgParser() {
+    }
+
+    ~ArgParser() {
+    }
+
+    void parse(const v8::FunctionCallbackInfo<v8::Value> & args, int index = 0) {
+    }
+};
+
+template<typename... Arguments>
+class ArgParser<int, Arguments...> : public ArgParser<Arguments...>
+{
+public:
+    ArgParser() {
+    }
+
+    ~ArgParser() {
+    }
+
+    void parse(const v8::FunctionCallbackInfo<v8::Value> & args, int index = 0) {
+        if (index < args.Length()) {
+            printf("int: %d\n", args[index]->Int32Value());
+        }
+
+        ArgParser<Arguments...>::parse(args, index+1);
+    }
+};
+
+template<typename... Arguments>
+class ArgParser<float, Arguments...> : public ArgParser<Arguments...>
+{
+public:
+    ArgParser() {
+    }
+
+    ~ArgParser() {
+    }
+
+    void parse(const v8::FunctionCallbackInfo<v8::Value> & args, int index = 0) {
+        if (index < args.Length()) {
+            printf("float: %.4f\n", args[index]->NumberValue());
+        }
+
+        ArgParser<Arguments...>::parse(args, index+1);
+    }
+};
+
+template<typename... Arguments>
+class ScriptFunc
+{
+public:
+    typedef std::function<void(Arguments...)> Function;
+
+    ArgParser<Arguments...> argParser;
+};
+
+
+static void testFunction(const v8::FunctionCallbackInfo<v8::Value> & args)
+{
+    //printf("Hello World!\n");
+
+    /*
+    for (int i=0; i<args.Length(); i++) {
+        v8::HandleScope scope(args.GetIsolate());
+
+        // String argument
+//      v8::String::AsciiValue str(args[i]);
+//      printf("%s\n", *str);
+
+        // Int argument
+        int num = args[i]->Int32Value();
+        printf("%d\n", num);
+    }
+    */
+
+    // Set return value
+    /*
+    v8::Handle<v8::String> source = ReadFile(args.GetIsolate(), *file);
+    args.GetReturnValue().Set(source);
+    */
+
+    ArgParser<int, float, float, float> argParser;
+    argParser.parse(args);
+}
+
+
 JSScriptEnvironment::JSScriptEnvironment()
 {
 	// Get isolate
     Isolate * isolate = Isolate::GetCurrent();
-    HandleScope handle_scope(isolate);
+    HandleScope scope(isolate);
+
+    // Create global object
+    Handle<ObjectTemplate> global = ObjectTemplate::New();
+    global->Set(String::New("test"), FunctionTemplate::New(testFunction));
 
 	// Create global context
-    Handle<Context> context = Context::New(isolate);
+    Handle<Context> context = Context::New(isolate, nullptr, global);
     m_context.Reset(isolate, context);
 }
 
@@ -29,7 +125,7 @@ void JSScriptEnvironment::evaluate(const std::string & code)
 {
 	// Get isolate
     Isolate * isolate = Isolate::GetCurrent();
-    HandleScope handle_scope(isolate);
+    HandleScope scope(isolate);
 
 	// Get global context
     Local<Context> context = Local<Context>::New(isolate, m_context);
