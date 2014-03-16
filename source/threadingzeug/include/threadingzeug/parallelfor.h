097e181807b77c8bf0427837c508bddbb8cc0330
Jan Ole Vollmer <jan.vollmer@student.hpi.uni-potsdam.de>
2014-03-16 11:26:57 +0100

    Fix compile warning on Windows: C4273: 'threadingzeug::parallel_for' : inconsistent dll linkage

--- source/threadingzeug/include/threadingzeug/parallelfor.h
+++ source/threadingzeug/include/threadingzeug/parallelfor.h
@@ -9,19 +9,19 @@ namespace threadingzeug
 {
 
 template<typename T>
-THREADINGZEUG_API void parallel_for(const std::vector<T>& elements, std::function<void(const T& element)> callback);
+void parallel_for(const std::vector<T>& elements, std::function<void(const T& element)> callback);
 
 template<typename T>
-THREADINGZEUG_API void parallel_for(std::vector<T>& elements, std::function<void(T& element)> callback);
+void parallel_for(std::vector<T>& elements, std::function<void(T& element)> callback);
 
 THREADINGZEUG_API void parallel_for(int start, int end, std::function<void(int i)> callback);
 
 
 template<typename T>
-THREADINGZEUG_API void sequential_for(const std::vector<T>& elements, std::function<void(const T& element)> callback);
+void sequential_for(const std::vector<T>& elements, std::function<void(const T& element)> callback);
 
 template<typename T>
-THREADINGZEUG_API void sequential_for(std::vector<T>& elements, std::function<void(T& element)> callback);
+void sequential_for(std::vector<T>& elements, std::function<void(T& element)> callback);
 
 THREADINGZEUG_API void sequential_for(int start, int end, std::function<void(int i)> callback);
 
