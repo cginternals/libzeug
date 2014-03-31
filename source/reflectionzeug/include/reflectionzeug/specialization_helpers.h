be4c35c67dfa8173c0981cec476ce8e8a56374b6
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2014-03-31 16:57:05 +0200

    possible alias templates visual studio fix

--- source/reflectionzeug/include/reflectionzeug/specialization_helpers.h
+++ source/reflectionzeug/include/reflectionzeug/specialization_helpers.h
@@ -7,10 +7,22 @@
 namespace reflectionzeug
 {
 
-template <typename Condition>
-using EnableIf = typename std::enable_if<Condition::value>::type;
+//template <typename Condition>
+//using EnableIf = typename std::enable_if<Condition::value>::type;
 
 template <typename Condition>
-using DisableIf = typename std::enable_if<!Condition::value>::type;
+struct EnableIf_h : public std::enable_if<Condition::value> {};
+    
+template <typename Condition>
+using EnableIf = typename EnableIf_h<Condition>::type;
+    
+//template <typename Condition>
+//using DisableIf = typename std::enable_if<!Condition::value>::type;
+    
+template <typename Condition>
+struct DisableIf_h : public std::enable_if<!Condition::value> {};
+
+template <typename Condition>
+using DisableIf = typename DisableIf_h<Condition>::type;
 
 } // namespace reflectionzeug
