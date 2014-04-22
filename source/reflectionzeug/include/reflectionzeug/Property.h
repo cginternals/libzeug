47b6965aacacf761fdc750cbd73e428d107d7ac4
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2014-04-22 19:26:29 +0200

    improve Property template documentation

--- source/reflectionzeug/include/reflectionzeug/Property.h
+++ source/reflectionzeug/include/reflectionzeug/Property.h
@@ -1,8 +1,6 @@
 
 #pragma once
 
-#include <array>
-
 #include <reflectionzeug/property_declaration.h>
 
 #include <reflectionzeug/BoolProperty.h>
@@ -19,8 +17,41 @@
 
 #include <reflectionzeug/specialization_helpers.h>
 
+
 namespace reflectionzeug
 {
+
+/** 
+ * \defgroup property_specializations Property Specializations
+ * \brief %All possible template specializations of Property
+ *
+ * Only this template class may be instantiated!
+ * Supported Types:
+ * - bool
+ * - double
+ * - float
+ * - std::string
+ * - FilePath
+ * - any integral type
+ * - any enum
+ * - any class that implements (e.g. Color):
+ *   \code{.cpp}
+ *   static Class fromString(std::string string, bool * ok);
+ *   std::string toString();
+ *   \endcode
+ * - any std::array of the above types
+ *
+ * The template specializations of supported types can have different super classes.
+ * They have additional meta information, utility methods and common interfaces specific to each type. 
+ * The specialization chosen for each type is determined via SFINAE (Substitution failure is not an error).
+ * If you are going to extend the property class, keep in mind that all specializations 
+ * have to somehow inherit from AbstractValueProperty. You will probably also have to extend your property visitor.
+ *
+ * \see \ref type_traits
+ * \see AbstractPropertyVisitor
+ * \see http://en.wikipedia.org/wiki/Substitution_failure_is_not_an_error
+ */
+/** \{ */
 
 template <typename Type, typename>
 class Property : public ClassProperty<Type>
@@ -30,7 +61,6 @@ public:
     Property(const std::string & name, Args&&... args) : 
         AbstractProperty(name),
         ClassProperty<Type>(std::forward<Args>(args)...) {}
-
 };
 
 template <>
@@ -41,7 +71,6 @@ public:
     Property(const std::string & name, Args&&... args) : 
         AbstractProperty(name),
         BoolProperty(std::forward<Args>(args)...) {}
-
 };
 
 template <>
@@ -52,7 +81,6 @@ public:
     Property(const std::string & name, Args&&... args) : 
         AbstractProperty(name),
         StringProperty(std::forward<Args>(args)...) {}
-
 };
 
 template <>
@@ -63,7 +91,6 @@ public:
     Property(const std::string & name, Args&&... args) : 
         AbstractProperty(name),
         FilePathProperty(std::forward<Args>(args)...) {}
-
 };
 
 template <typename Type>
@@ -74,7 +101,6 @@ public:
     Property(const std::string & name, Args&&... args) : 
         AbstractProperty(name),
         UnsignedIntegralProperty<Type>(std::forward<Args>(args)...) {}
-
 };
 
 template <typename Type>
@@ -85,7 +111,6 @@ public:
     Property(const std::string & name, Args&&... args) : 
         AbstractProperty(name),
         SignedIntegralProperty<Type>(std::forward<Args>(args)...) {}
-
 };
 
 template <typename Type>
@@ -96,9 +121,8 @@ public:
     Property(const std::string & name, Args&&... args) : 
         AbstractProperty(name), 
         FloatingPointProperty<Type>(std::forward<Args>(args)...) {}
-
 };
-    
+
 template <typename Type>
 class Property<Type, EnableIf<isArray<Type>>> : public ArrayProperty<typename Type::value_type, std::tuple_size<Type>::value>
 {
@@ -111,7 +135,6 @@ public:
     Property(const std::string & name, Args&&... args) : 
         AbstractProperty(name),
         ArrayProperty<typename Type::value_type, std::tuple_size<Type>::value>(std::forward<Args>(args)...) {}
-    
 };
 
 template <typename Type>
@@ -122,7 +145,8 @@ public:
     Property(const std::string & name, Args&&... args) : 
         AbstractProperty(name),
         EnumProperty<Type>(std::forward<Args>(args)...) {}
-    
 };
+
+/** \} */
 
 } // namespace reflectionzeug
--- source/reflectionzeug/include/reflectionzeug/property_declaration.h
+++ source/reflectionzeug/include/reflectionzeug/property_declaration.h
@@ -5,25 +5,6 @@
 namespace reflectionzeug
 {
 
-/** 
- * \brief The template class of all instantiated properties.
- *
- * Only this class may be instantiated!
- * Supported Types:
- * - bool
- * - double
- * - float
- * - std::string
- * - FilePath
- * - any integral type
- * - any enum
- * - any class that implements (e.g. Color):
- *   \code{.cpp}
- *   static Class fromString(std::string string, bool * ok);
- *   std::string toString();
- *   \endcode
- * - any std::array of the above types
- */
 template <typename Type, typename = void>
 class Property;
 
