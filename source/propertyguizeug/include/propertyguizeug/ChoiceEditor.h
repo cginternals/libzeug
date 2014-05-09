519909911a70d0ddb5e2c62909f34e1481bf6bb8
Stefan Buschmann <stefan.buschmann@hpi.uni-potsdam.de>
2014-05-09 23:37:02 +0200

    update combo box, when the choices of a string property have been changed

--- source/propertyguizeug/include/propertyguizeug/ChoiceEditor.h
+++ source/propertyguizeug/include/propertyguizeug/ChoiceEditor.h
@@ -5,6 +5,8 @@
 
 #include <propertyguizeug/PropertyEditor.h>
 
+class QComboBox;
+
 namespace propertyguizeug
 {
 
@@ -13,11 +15,15 @@ class PROPERTYGUIZEUG_API ChoiceEditor : public PropertyEditor
 public:
     ChoiceEditor(reflectionzeug::Property<std::string> * property, QWidget * parent = nullptr);
     virtual ~ChoiceEditor();
-    
+
     void setString(const QString & text);
 
 protected:
+    void propertyChoicesChanged(const std::vector<std::string> & choices);
+
+protected:
     reflectionzeug::Property<std::string> * m_property;
+    QComboBox * m_comboBox;
 };
 
 } // namespace propertyguizeug
