b41df1674d96bde20ab0ccee61e501afa2735dec
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2014-07-21 15:16:47 +0200

    add always-expand-groups option to propertybrowser

--- source/propertyguizeug/source/PropertyBrowser.cpp
+++ source/propertyguizeug/source/PropertyBrowser.cpp
@@ -2,7 +2,6 @@
 #include <cassert>
 
 #include <reflectionzeug/AbstractProperty.h>
-#include <reflectionzeug/AbstractValueProperty.h>
 
 #include <propertyguizeug/PropertyModel.h>
 #include <propertyguizeug/PropertyDelegate.h>
@@ -53,8 +52,9 @@ PropertyBrowser::PropertyBrowser(
     QWidget * parent)
 :   QTreeView(parent)
 ,   m_delegate(new PropertyDelegate(editorFactory, painter, this))
+,   m_alwaysExpandGroups(false)
 {
-    this->setModel(nullptr);
+    this->setRoot(nullptr);
     this->setItemDelegateForColumn(1, m_delegate);
     
     initView();
@@ -67,8 +67,9 @@ PropertyBrowser::PropertyBrowser(
     QWidget * parent)
 :   QTreeView(parent)
 ,   m_delegate(new PropertyDelegate(editorFactory, painter, this))
+,   m_alwaysExpandGroups(false)
 {
-    this->setModel(new PropertyModel(root));
+    this->setRoot(root);
     this->setItemDelegateForColumn(1, m_delegate);
     
     initView();
@@ -81,9 +82,42 @@ PropertyBrowser::~PropertyBrowser()
     
 void PropertyBrowser::setRoot(reflectionzeug::PropertyGroup * root)
 {   
+    QAbstractItemModel * newModel = (root == nullptr) ? nullptr : new PropertyModel(root);
+
     QAbstractItemModel * model = this->model();
-    this->setModel((root == nullptr) ? nullptr : new PropertyModel(root));
+    this->setModel(newModel);
     delete model;
+
+    if (m_alwaysExpandGroups && newModel)
+    {
+        connect(newModel, &QAbstractItemModel::rowsInserted,
+                this, &PropertyBrowser::onRowsInserted);
+
+        this->expandAllGroups();
+    }
+}
+
+void PropertyBrowser::onRowsInserted(const QModelIndex & parentIndex, int first, int last)
+{
+    QAbstractItemModel * model = this->model();
+
+    for (int i = first; i <= last; ++i)
+    {
+        QModelIndex index = model->index(i, 0, parentIndex);
+
+        if (!index.isValid())
+            continue;
+
+        AbstractProperty * property = retrieveProperty(index);
+
+        if (property->isGroup() && model->hasChildren(index))
+            this->expand(index);
+    }
+}
+
+void PropertyBrowser::setAlwaysExpandGroups(bool b)
+{
+    m_alwaysExpandGroups = b;
 }
     
 void PropertyBrowser::initView()
@@ -92,6 +126,23 @@ void PropertyBrowser::initView()
     this->setAlternatingRowColors(true);
     this->setUniformRowHeights(true);
     this->setTabKeyNavigation(true);
+}
+
+void PropertyBrowser::expandAllGroups()
+{
+    QAbstractItemModel * model = this->model();
+
+    QModelIndexList indexes = model->match(model->index(0,0), Qt::DisplayRole, "*", -1, Qt::MatchWildcard|Qt::MatchRecursive);
+    for (QModelIndex index : indexes)
+    {
+        if (!index.isValid())
+            continue;
+
+        AbstractProperty * property = retrieveProperty(index);
+
+        if (property->isGroup() && model->hasChildren(index))
+            this->expand(index);
+    }
 }
 
 } // namespace propertyguizeug
