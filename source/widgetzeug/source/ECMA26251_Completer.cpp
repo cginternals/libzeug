2f814d4da90c7586a694be066006f0a68ac1e9b1
Daniel <Daniel.Limberger@fb03h222ws03.hpi.uni-potsdam.de>
2014-11-24 11:49:55 +0100

    move script related widgets from glwhite to widgetzeug

---
+++ source/widgetzeug/source/ECMA26251_Completer.cpp
@@ -0,0 +1,51 @@
+
+#include <widgetzeug/ECMA26251_Completer.h>
+
+#include <QString>
+
+namespace widgetzeug
+{
+
+namespace
+{
+    // http://www.ecma-international.org/ecma-262/5.1/
+
+    const QStringList ECMA_Keywords = QStringList() 
+        << "break"
+        << "case"
+        << "catch"
+        << "continue"
+        << "debugger"
+        << "default"
+        << "delete"
+        << "do"
+        << "else"
+        << "finally"
+        << "for"
+        << "function"
+        << "if"
+        << "in"
+        << "instanceof"
+        << "new"
+        << "return"
+        << "switch"
+        << "this"
+        << "throw"
+        << "try"
+        << "typeof"
+        << "var"
+        << "void"
+        << "while"
+        << "with";
+}
+
+ECMA26251Completer::ECMA26251Completer()
+{
+    registerWords(ECMA_Keywords);
+};
+
+ECMA26251Completer::~ECMA26251Completer()
+{
+}
+
+} // namespace widgetzeug
