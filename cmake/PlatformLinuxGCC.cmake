38c810958ba22b406ddddae687102fc7594f6e99
Daniel Limberger <Daniel.Limberger@fb03h222ws03.hpi.uni-potsdam.de>
2014-11-28 16:41:16 +0100

    allow for release and debug specific compile flags using cmake generator expression

--- cmake/PlatformLinuxGCC.cmake
+++ cmake/PlatformLinuxGCC.cmake
@@ -35,26 +35,44 @@ else()
     set(EXCEPTION_FLAG "-fno-exceptions")
 endif()
 
-set(LINUX_COMPILE_FLAGS "-pthread -pipe -fPIC -Wreturn-type -Wall -Wextra -Wcast-align -Wno-unused-parameter -Wno-unused-variable ${EXCEPTION_FLAG}")
-# pthread       -> use pthread library
-# no-rtti       -> disable c++ rtti
-# no-exceptions -> disable exception handling
-# pipe          -> use pipes
-# fPIC          -> use position independent code
-# -Wreturn-type -Werror=return-type -> missing returns in functions and methods are handled as errors which stops the compilation
-# -Wshadow -> e.g. when a parameter is named like a member, too many warnings, disabled for now
+set(LINUX_COMPILE_FLAGS 
+      
+      ${EXCEPTION_FLAG}
+      -pthread      # -> use pthread library
+    # -no-rtti      # -> disable c++ rtti
+      -pipe         # -> use pipes
+      -Wall         # -> 
+      -Wextra       # -> 
+      -Werror       # ->
+      -fPIC         # -> use position independent code
 
-if(CMAKE_COMPILER_IS_GNUCXX)
-    # gcc
-    set(LINUX_COMPILE_FLAGS "${LINUX_COMPILE_FLAGS} -Wno-unused-but-set-variable")
-elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+      -Wreturn-type 
+      -Wcast-align  
+      -Wno-unused-parameter
+      -Wno-unused-variable
+      -Wno-unused-but-set-variable
+
+    # -Werror=return-type -> missing returns in functions and methods are handled as errors which stops the compilation
+    # -Wshadow      # -> e.g. when a parameter is named like a member, too many warnings, disabled for now
+)
+
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
     # clang
-    set(LINUX_COMPILE_FLAGS "${LINUX_COMPILE_FLAGS} -Wno-reinterpret-base-class -Wno-overloaded-virtual")
+    set(LINUX_COMPILE_FLAGS ${LINUX_COMPILE_FLAGS} 
+        -Wno-reinterpret-base-class 
+        -Wno-overloaded-virtual
+    )
 endif()
 
-set(LINUX_LINKER_FLAGS "-pthread")
+set(DEFAULT_COMPILE_FLAGS
+    ${LINUX_COMPILE_FLAGS}
+    $<$<CONFIG:Debug>:   
+    >
+    $<$<CONFIG:Release>: 
+    >
+)
 
-set(DEFAULT_COMPILE_FLAGS ${LINUX_COMPILE_FLAGS})
+set(LINUX_LINKER_FLAGS "-pthread")
 
 set(DEFAULT_LINKER_FLAGS_RELEASE ${LINUX_LINKER_FLAGS})
 set(DEFAULT_LINKER_FLAGS_DEBUG ${LINUX_LINKER_FLAGS})
