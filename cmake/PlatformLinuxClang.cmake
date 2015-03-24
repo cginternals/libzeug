735b18f75f9f9f8274a755f34a1390d8839aab3d
Willy Scheibel <willyscheibel@gmx.de>
2015-03-24 13:43:17 +0100

    Use correct std lib for clang on Linux

--- cmake/PlatformLinuxClang.cmake
+++ cmake/PlatformLinuxClang.cmake
@@ -3,7 +3,7 @@ message(STATUS "Configuring for platform Linux/Clang.")
 
 # Enable C++11 support
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++")
 
 include_directories("/usr/include/c++/v1/")
 
