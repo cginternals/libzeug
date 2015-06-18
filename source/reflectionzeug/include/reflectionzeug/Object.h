3800c92f6578858862b169b6beb5290003b5fc4d
Stefan Buschmann <stefan.buschmann@hpi.uni-potsdam.de>
2015-06-18 12:51:00 +0200

    Documentation

--- source/reflectionzeug/include/reflectionzeug/Object.h
+++ source/reflectionzeug/include/reflectionzeug/Object.h
@@ -12,17 +12,56 @@ namespace reflectionzeug
 {
 
 
-/** \brief Base class for reflectable classes that can expose properties and functions
- */
+/**
+*  @brief
+*    Base  class for reflectable classes that can expose properties and functions
+*
+*    To export one of your classes for automatic GUI generation and scripting, the
+*    recommended usage is to inherit from Object and register your properties and
+*    scripting functions in the constructor.
+*/
 class REFLECTIONZEUG_API Object : public PropertyGroup
 {
 public:
+    /**
+    *  @brief
+    *    Constructor
+    */
     Object();
+
+    /**
+    *  @brief
+    *    Constructor
+    *
+    *  @param[in] name
+    *    Name of object (for scripting and property access)
+    */
     Object(const std::string & name);
+
+    /**
+    *  @brief
+    *    Destructor
+    */
     virtual ~Object();
 
+    /**
+    *  @brief
+    *    Get list of functions exported by this object
+    *
+    *  @return
+    *    List of functions
+    */
     const std::vector<AbstractFunction *> & functions() const;
 
+    /**
+    *  @brief
+    *    Add (export) function on object
+    *
+    *  @param[in] name
+    *    Function name
+    *  @param[in] fn
+    *    Function pointer
+    */
     template <typename RET, typename... Arguments>
     void addFunction(const std::string & name, RET (*fn)(Arguments...))
     {
@@ -30,6 +69,17 @@ public:
         m_functions.push_back(func);
     }
 
+    /**
+    *  @brief
+    *    Add (export) method on object
+    *
+    *  @param[in] name
+    *    Function name
+    *  @param[in] obj
+    *    Pointer to object instance
+    *  @param[in] member
+    *    Method pointer
+    */
     template <class T, typename RET, typename... Arguments>
     void addFunction(const std::string & name, T * obj, RET (T::*member)(Arguments...))
     {
@@ -39,7 +89,7 @@ public:
 
 
 protected:
-    std::vector<AbstractFunction *> m_functions;
+    std::vector<AbstractFunction *> m_functions;    /**< List of exported functions */
 };
 
 
--- source/reflectionzeug/include/reflectionzeug/variant/Variant.h
+++ source/reflectionzeug/include/reflectionzeug/variant/Variant.h
@@ -17,30 +17,77 @@ class AbstractAccessor;
 class Variant;
 
 
+/**
+*  @brief
+*    Variant array (analog to a JSON array)
+*/
 using VariantArray = std::vector<Variant>;
+
+/**
+*  @brief
+*    Variant map (analog to a JSON object)
+*/
 using VariantMap = std::map<std::string, Variant>;
 
 
 /**
 *  @brief
 *    Variant typed value
+*
+*    A Variant can be used to store and pass arbitrary typed values in a uniform manner.
+*
+*    To create a Variant from the list of supported primitive data types, use one of the
+*    available constructors. To create a Variant from another data type, use the static
+*    function fromValue().
+*
+*    The type of the stored value can be determined by type() or hasType<Type>().
+*    To access the value of a variant, call value<Type>(). If the given data type does not
+*    match the stored type, the value may be converted. Note that this only works for the
+*    supported primitive data types. To check if a type can be converted, use canConvert<Type>().
+*
+*    VariantArray and VariantMap can be used to store collections or hierarchies of variants.
+*    For simplified usage, use the convenience functions array() or map() to create such variants,
+*    and toArray() or toMap() to access their data. These composite variants will automatically
+*    be interpreted as JSON arrays or objects within scriptzeug and can be serialized by the
+*    JSON tool class.
 */
 class REFLECTIONZEUG_API Variant
 {
 public:
+    /**
+    *  @brief
+    *    Create a variant from an arbitrary typed value
+    *
+    *  @param[in] value
+    *    Value
+    *
+    *  @return
+    *    Variant instance
+    */
     template <typename ValueType>
     static Variant fromValue(const ValueType & value);
 
-    /** Returns Variant that stores an empty VariantArray.
-     */
+    /**
+    *  @brief
+    *    Create an empty variant array
+    *
+    *  @return
+    *    Variant instance
+    */
     static Variant array();
 
-    /** Returns Variant that stores an empty VariantMap. 
-     */
+    /**
+    *  @brief
+    *    Create an empty variant map
+    *
+    *  @return
+    *    Variant instance
+    */
     static Variant map();
 
 
 public:
+    //@{
     /**
     *  @brief
     *    Constructor for an empty value
@@ -55,7 +102,16 @@ public:
     *    Variant whose value will be copied
     */
     Variant(const Variant & variant);
+    //@}
 
+    /**
+    *  @brief
+    *    Constructor for a primitive data type
+    *
+    *  @param[in] value
+    *    Primitive value
+    */
+    //@{
     Variant(bool value);
     Variant(char value);
     Variant(unsigned char value);
@@ -74,63 +130,149 @@ public:
     Variant(const std::vector<std::string> & value);
     Variant(const VariantArray & array);
     Variant(const VariantMap & map);
+    //@}
 
+    //@{
     /**
     *  @brief
     *    Destructor
     */
     virtual ~Variant();
 
+    /**
+    *  @brief
+    *    Copy operator
+    *
+    *  @param[in] variant
+    *    Variant whose value will be copied
+    *
+    *  @return
+    *    Variant
+    */
     Variant & operator=(const Variant & variant);
 
+    /**
+    *  @brief
+    *    Check if variant is empty
+    *
+    *  @return
+    *    'true' if variant does not contain a value, else 'false'
+    */
     bool isNull() const;
+
+    /**
+    *  @brief
+    *    Check if variant contains an array
+    *
+    *  @return
+    *    'true' if variant has type VariantArray, else 'false'
+    */
     bool isArray() const;
+
+    /**
+    *  @brief
+    *    Check if variant contains a map
+    *
+    *  @return
+    *    'true' if variant has type VariantMap, else 'false'
+    */
     bool isMap() const;
 
+    /**
+    *  @brief
+    *    Get type of variant value
+    *
+    *  @return
+    *    Type id
+    */
     const std::type_info & type() const;
 
-    /** Returns true if the Variant has the template type ValueType.
-     */
+    /**
+    *  @brief
+    *    Check type of variant value
+    *
+    *  @return
+    *    'true' if variant value has the template type ValueType, else 'false'
+    */
     template <typename ValueType>
     bool hasType() const;
 
-    /** Returns true if the Variant has or
-     * can be converted to the template type ValueType.
-     * \see registerConverter()
-     */
+    /**
+    *  @brief
+    *    Check if variant type can be converted
+    *
+    *  @return
+    *    'true' if variant can be converted into type ValueType, else 'false'.
+    *
+    *  @remarks
+    *    Conversion is only supported for the following primitive data types:
+    *      bool, char, unsigned char, short, unsigned short, int, unsigned int,
+    *      long, unsigned long, long long, unsigned long long, float, double,
+    *      std::string
+    */
     template <typename ValueType>
     bool canConvert() const;
 
-    /** Returns the stored value converted to the template type ValueType. 
-     * Call canConvert() to find out whether a type can be converted. 
-     * If the value cannot be converted, a default-constructed value will be returned.
-     */
+    /**
+    *  @brief
+    *    Get stored value
+    *
+    *  @param[in] defaultValue
+    *    Default value that is returned if the value cannot be returned or converted
+    *
+    *  @return
+    *    Stored value of type ValueType, or defaultValue if type does not match and cannot be converted
+    */
     template <typename ValueType>
     ValueType value(const ValueType & defaultValue = ValueType()) const;
 
-    /** Returns a pointer to the stored value if it has the template type ValueType.
-     * Otherwise returns nullptr.
-     */
+    //@{
+    /**
+    *  @brief
+    *    Get pointer to the stored value
+    *
+    *  @return
+    *    Pointer to stored value of type ValueType, or nullptr if type does not match and cannot be converted
+    */
     template <typename ValueType>
     ValueType * ptr();
     template <typename ValueType>
     const ValueType * ptr() const;
+    //@}
 
-    /** Convenience method. Does the same as calling 
-     * \code variant.ptr<VariantArray>() \endcode
-     */
+    //@{
+    /**
+    *  @brief
+    *    Get pointer to the stored VariantArray
+    *
+    *  @return
+    *    Pointer to VariantArray, or nullptr if the variant does not contain a variant array
+    *
+    *  @remarks
+    *    Does the same as calling \code variant.ptr<VariantArray>() \endcode
+    */
     VariantArray * toArray();
     const VariantArray * toArray() const;
+    //@}
 
-    /** Convenience method. Does the same as calling
-     * \code variant.ptr<VariantMap>() \endcode
-     */
+    //@{
+    /**
+    *  @brief
+    *    Get pointer to the stored VariantMap
+    *
+    *  @return
+    *    Pointer to VariantMap, or nullptr if the variant does not contain a variant map
+    *
+    *  @remarks
+    *    Does the same as calling \code variant.ptr<VariantMap>() \endcode
+    */
     VariantMap * toMap();
     const VariantMap * toMap() const;
+    //@}
 
 
 protected:
-    AbstractAccessor * m_accessor;    /**< Variant value */
+    AbstractAccessor * m_accessor;    /**< Value accessor, can be nullptr */
 };
 
 
