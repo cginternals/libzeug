fc4f02950b440573cf9d4a26278cacac84883f84
Stefan Buschmann <stefan.buschmann@hpi.uni-potsdam.de>
2015-06-21 17:31:41 +0200

    Implement property class for QColor

--- source/reflectionzeug/include/reflectionzeug/extensions/QtProperties.hpp
+++ source/reflectionzeug/include/reflectionzeug/extensions/QtProperties.hpp
@@ -3,6 +3,7 @@
 
 
 #include <QString>
+#include <QColor>
 
 #include <reflectionzeug/property/Property.h>
 
@@ -67,12 +68,143 @@ public:
 
 /**
 *  @brief
+*    Property implementation for QColor
+*/
+class PropertyQColor : public reflectionzeug::AbstractValueProperty<QColor>, public AbstractColorInterface
+{
+public:
+    /**
+    *  @brief
+    *    Constructor
+    */
+    template <typename... Arguments>
+    PropertyQColor(Arguments&&... args)
+    : reflectionzeug::AbstractValueProperty<QColor>(std::forward<Arguments>(args)...)
+    {
+    }
+
+    // Virtual AbstractColorInterface interface
+    virtual void getRGBA(int & r, int & g, int & b, int & a) const override
+    {
+        QColor color = this->value();
+        r = color.red();
+        g = color.green();
+        b = color.blue();
+        a = color.alpha();
+    }
+
+    virtual void setRGBA(int r, int g, int b, int a) override
+    {
+        this->setValue(QColor(r, g, b, a));
+    }
+
+    virtual int red() const override
+    {
+        return this->value().red();
+    }
+
+    virtual void setRed(int red) override
+    {
+        QColor color = this->value();
+        color.setRed(red);
+        this->setValue(color);
+    }
+
+    virtual int green() const override
+    {
+        return this->value().green();
+    }
+
+    virtual void setGreen(int green) override
+    {
+        QColor color = this->value();
+        color.setGreen(green);
+        this->setValue(color);
+    }
+
+    virtual int blue() const override
+    {
+        return this->value().blue();
+    }
+
+    virtual void setBlue(int blue) override
+    {
+        QColor color = this->value();
+        color.setBlue(blue);
+        this->setValue(color);
+    }
+
+    virtual int alpha() const override
+    {
+        return this->value().alpha();
+    }
+
+    virtual void setAlpha(int alpha) override
+    {
+        QColor color = this->value();
+        color.setAlpha(alpha);
+        this->setValue(color);
+    }
+
+
+    // Virtual AbstractProperty interface
+    virtual std::string toString() const override 
+    {
+        // Return hex representation
+        return toHexString();
+    }
+
+    virtual bool fromString(const std::string & string) override
+    {
+        // Read from hex representation
+        return fromHexString(string);
+    }
+
+    virtual Variant toVariant() const override
+    {
+        // Return color as variant object
+        return toColorVariant();
+    }
+
+    virtual bool fromVariant(const Variant & value) override
+    {
+        // Read from variant of the exact type
+        if (value.hasType<QColor>() || value.canConvert<QColor>()) {
+            setValue( value.value<QColor>() );
+            return true;
+        }
+
+        // Read from string or object representation
+        else {
+            return fromColorVariant(value);
+        }
+    }
+
+    virtual void accept(AbstractVisitor * visitor) override
+    {
+        visitor->callVisitor<PropertyQColor>(this);
+        visitor->callVisitor<AbstractColorInterface>(this);
+    }
+};
+
+/**
+*  @brief
 *    Property selector for properties of type QString
 */
 template <>
 struct PropertyTypeSelector<QString>
 {
     using Type = PropertyQString;
+};
+
+/**
+*  @brief
+*    Property selector for properties of type QColor
+*/
+template <>
+struct PropertyTypeSelector<QColor>
+{
+    using Type = PropertyQColor;
 };
 
 
