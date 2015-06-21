2e47b0615b7e9323a9f10f44bd85fcca4ded9402
Stefan Buschmann <stefan.buschmann@hpi.uni-potsdam.de>
2015-06-21 18:43:11 +0200

    Remove V8 scripting backend

--- source/scriptzeug/source/ScriptContext.cpp
+++ source/scriptzeug/source/ScriptContext.cpp
@@ -1,11 +1,9 @@
+
 #include <scriptzeug/ScriptContext.h>
+
 #include <scriptzeug/backend/AbstractScriptContext.h>
 
 #include "backend-duktape/DuktapeScriptContext.h"
-
-#ifdef LIBZEUG_USE_V8
-    #include "backend-v8/V8ScriptContext.h"
-#endif
 
 
 using namespace reflectionzeug;
@@ -18,19 +16,12 @@ ScriptContext::ScriptContext(const std::string & backend)
 {
     // Create backend
 
-    // Javascript (default: duktape)
+    // Javascript (duktape)
 
     // Duktape
     if (backend == "duktape" || backend == "javascript" || backend == "js") {
         m_backend = new DuktapeScriptContext(this);
     }
-
-    // V8
-#ifdef LIBZEUG_USE_V8
-    else if (backend == "v8") {
-        m_backend = new V8ScriptContext(this);
-    }
-#endif
 }
 
 ScriptContext::~ScriptContext()
