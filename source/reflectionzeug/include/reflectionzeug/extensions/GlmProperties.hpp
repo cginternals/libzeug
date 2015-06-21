d5ce9e769bd86f45237c13769eee91ac676971d8
Stefan Buschmann <stefan.buschmann@hpi.uni-potsdam.de>
2015-06-21 11:52:33 +0200

    Refine GLM property extensions: Use class template for all vector types, as the implementation of each class is similar. Implement toVariant/fromVariant. Add documentation.

--- source/reflectionzeug/include/reflectionzeug/extensions/GlmProperties.hpp
+++ source/reflectionzeug/include/reflectionzeug/extensions/GlmProperties.hpp
@@ -15,14 +15,24 @@ namespace reflectionzeug
 {
 
 
-template <typename T, unsigned Count>
+/**
+*  @brief
+*    Convert glm vector into string
+*
+*  @param[in] data
+*    Pointer to vector data
+*
+*  @return
+*    String representation
+*/
+template <typename T, unsigned Size>
 std::string glmToString(const T * data)
 {
     std::stringstream ss;
 
     ss << "(";
 
-    for (unsigned i = 0; i < Count; ++i)
+    for (unsigned i = 0; i < Size; ++i)
     {
         if (i > 0)
             ss << ", ";
@@ -35,14 +45,26 @@ std::string glmToString(const T * data)
     return ss.str();
 }
 
-template <typename T, unsigned Count>
+/**
+*  @brief
+*    Convert string into glm vector
+*
+*  @param[in] string
+*    String representation
+*  @param[in] data
+*    Pointer to vector data
+*
+*  @return
+*    'true' if all went fine, else 'false'
+*/
+template <typename T, unsigned Size>
 bool glmFromString(const std::string & string, T * data)
 {
     std::string elementRegex = std::is_integral<T>::value ? "(-|\\+)?\\d+" : "(-|\\+)?\\d+\\.?\\d*";
 
     std::stringstream ss;
     ss << "\\s*\\(";
-    for (unsigned i=0; i<Count-1; ++i)
+    for (unsigned i=0; i<Size-1; ++i)
     {
         ss << "(" << elementRegex << ")";
         ss << "\\s*,\\s*";
@@ -54,9 +76,9 @@ bool glmFromString(const std::string & string, T * data)
 
     std::vector<std::string> parts = reflectionzeug::util::extract(string, elementRegex);
 
-    assert(parts.size() == Count);
+    assert(parts.size() == Size);
 
-    for (unsigned i = 0; i < Count; ++i)
+    for (unsigned i = 0; i < Size; ++i)
     {
         const std::string & part = parts[i];
         data[i] = std::is_integral<T>::value ? static_cast<T>(std::stoi(part)) : static_cast<T>(std::stod(part));
@@ -65,178 +87,139 @@ bool glmFromString(const std::string & string, T * data)
     return true;
 }
 
-class Vec2Property : public reflectionzeug::AbstractValueProperty<glm::vec2>
+/**
+*  @brief
+*    Property implementation for GLM vector types
+*/
+template <typename VectorType, typename ValueType, unsigned Size>
+class PropertyGlmVec : public reflectionzeug::AbstractValueProperty<VectorType>
 {
 public:
+    /**
+    *  @brief
+    *    Constructor
+    */
     template <typename... Arguments>
-    Vec2Property(Arguments&&... args)
-    : reflectionzeug::AbstractValueProperty<glm::vec2>(std::forward<Arguments>(args)...) {}
+    PropertyGlmVec(Arguments&&... args)
+    : reflectionzeug::AbstractValueProperty<VectorType>(std::forward<Arguments>(args)...)
+    {
+    }
 
+    // Virtual AbstractProperty interface
     virtual std::string toString() const override 
     {
-        return glmToString<glm::vec2::value_type, 2>(glm::value_ptr(this->value())); 
+        return glmToString<ValueType, Size>(glm::value_ptr(this->value())); 
     }
 
     virtual bool fromString(const std::string & string) override
     {
-        glm::vec2 value;
-        if (!glmFromString<glm::vec2::value_type, 2>(string, glm::value_ptr(value)))
+        VectorType value;
+        if (!glmFromString<ValueType, Size>(string, glm::value_ptr(value)))
             return false;
 
-        setValue(value);
+        this->setValue(value);
         return true;
     }
-};
 
-class IVec2Property : public reflectionzeug::AbstractValueProperty<glm::ivec2>
-{
-public:
-    template <typename... Arguments>
-    IVec2Property(Arguments&&... args)
-    : reflectionzeug::AbstractValueProperty<glm::ivec2>(std::forward<Arguments>(args)...) {}
-
-    virtual std::string toString() const override 
-    { 
-        return glmToString<glm::ivec2::value_type, 2>(glm::value_ptr(value())); 
-    }
-
-    virtual bool fromString(const std::string & string) override
+    virtual Variant toVariant() const override
     {
-        glm::ivec2 value;
-        if (!glmFromString<glm::ivec2::value_type, 2>(string, glm::value_ptr(value)))
-            return false;
-
-        setValue(value);
-        return true;
-    }
-};
-
-class Vec3Property : public reflectionzeug::AbstractValueProperty<glm::vec3>
-{
-public:
-    template <typename... Arguments>
-    Vec3Property(Arguments&&... args)
-    : reflectionzeug::AbstractValueProperty<glm::vec3>(std::forward<Arguments>(args)...) {}
-
-    virtual std::string toString() const override 
-    { 
-        return glmToString<glm::vec3::value_type, 3>(glm::value_ptr(value()));
+        // Return variant array
+        VectorType vector = this->value();
+        Variant array = Variant::array();
+        for (size_t i=0; i<Size; i++) {
+            array.toArray()->push_back(vector[i]);
+        }
+        return array;
     }
 
-    virtual bool fromString(const std::string & string) override
+    virtual bool fromVariant(const Variant & value) override
     {
-        glm::vec3 value;
-        if (!glmFromString<glm::vec3::value_type, 3>(string, glm::value_ptr(value)))
-            return false;
+        // Read from variant of the exact type
+        if (value.hasType<VectorType>() || value.canConvert<VectorType>()) {
+            this->setValue( value.value<VectorType>() );
+            return true;
+        }
 
-        setValue(value);
-        return true;
+        // Read from string
+        else if (value.hasType<std::string>() || value.canConvert<std::string>()) {
+            this->fromString( value.value<std::string>() );
+            return true;
+        }
+
+        // Read from variant array
+        else if (value.hasType<VariantArray>()) {
+            const VariantArray & array = *(value.toArray());
+            VectorType vector;
+            for (size_t i=0; i<Size; i++) {
+                vector[i] = array[i].value<ValueType>();
+            }
+            this->setValue(vector);
+            return true;
+        }
+
+        // Invalid value
+        return false;
     }
 };
 
-class IVec3Property : public reflectionzeug::AbstractValueProperty<glm::ivec3>
-{
-public:
-    template <typename... Arguments>
-    IVec3Property(Arguments&&... args)
-    : reflectionzeug::AbstractValueProperty<glm::ivec3>(std::forward<Arguments>(args)...) {}
-
-    virtual std::string toString() const override 
-    { 
-        return glmToString<glm::ivec3::value_type, 3>(glm::value_ptr(value())); 
-    }
-
-    virtual bool fromString(const std::string & string) override
-    {
-        glm::ivec3 value;
-        if (!glmFromString<glm::ivec3::value_type, 3>(string, glm::value_ptr(value)))
-            return false;
-
-        setValue(value);
-        return true;
-    }
-};
-
-class Vec4Property : public reflectionzeug::AbstractValueProperty<glm::vec4>
-{
-public:
-    template <typename... Arguments>
-    Vec4Property(Arguments&&... args)
-    : reflectionzeug::AbstractValueProperty<glm::vec4>(std::forward<Arguments>(args)...) {}
-
-    virtual std::string toString() const override 
-    { 
-        return glmToString<glm::vec4::value_type, 4>(glm::value_ptr(value())); 
-    }
-
-    virtual bool fromString(const std::string & string) override
-    {
-        glm::vec4 value;
-        if (!glmFromString<glm::vec4::value_type, 4>(string, glm::value_ptr(value)))
-            return false;
-
-        setValue(value);
-        return true;
-    }
-};
-
-class IVec4Property : public reflectionzeug::AbstractValueProperty<glm::ivec4>
-{
-public:
-    template <typename... Arguments>
-    IVec4Property(Arguments&&... args)
-    : reflectionzeug::AbstractValueProperty<glm::ivec4>(std::forward<Arguments>(args)...) {}
-
-    virtual std::string toString() const override 
-    { 
-        return glmToString<glm::ivec4::value_type, 4>(glm::value_ptr(value())); 
-    }
-
-    virtual bool fromString(const std::string & string) override
-    {
-        glm::ivec4 value;
-        if (!glmFromString<glm::ivec4::value_type, 4>(string, glm::value_ptr(value)))
-            return false;
-
-        setValue(value);
-        return true;
-    }
-};
-
+/**
+*  @brief
+*    Property selector for properties of type glm::vec2
+*/
 template <>
 struct PropertyTypeSelector<glm::vec2>
 {
-    using Type = Vec2Property;
+    using Type = PropertyGlmVec<glm::vec2, glm::vec2::value_type, 2>;
 };
 
+/**
+*  @brief
+*    Property selector for properties of type glm::ivec2
+*/
 template <>
 struct PropertyTypeSelector<glm::ivec2>
 {
-    using Type = IVec2Property;
+    using Type = PropertyGlmVec<glm::ivec2, glm::ivec2::value_type, 2>;
 };
 
+/**
+*  @brief
+*    Property selector for properties of type glm::vec3
+*/
 template <>
 struct PropertyTypeSelector<glm::vec3>
 {
-    using Type = Vec3Property;
+    using Type = PropertyGlmVec<glm::vec3, glm::vec3::value_type, 3>;
 };
 
+/**
+*  @brief
+*    Property selector for properties of type glm::ivec3
+*/
 template <>
 struct PropertyTypeSelector<glm::ivec3>
 {
-    using Type = IVec3Property;
+    using Type = PropertyGlmVec<glm::ivec3, glm::ivec3::value_type, 3>;
 };
 
+/**
+*  @brief
+*    Property selector for properties of type glm::ivec4
+*/
 template <>
 struct PropertyTypeSelector<glm::vec4>
 {
-    using Type = Vec4Property;
+    using Type = PropertyGlmVec<glm::vec4, glm::vec4::value_type, 4>;
 };
 
+/**
+*  @brief
+*    Property selector for properties of type glm::ivec4
+*/
 template <>
 struct PropertyTypeSelector<glm::ivec4>
 {
-    using Type = IVec4Property;
+    using Type = PropertyGlmVec<glm::ivec4, glm::ivec4::value_type, 4>;
 };
 
 
