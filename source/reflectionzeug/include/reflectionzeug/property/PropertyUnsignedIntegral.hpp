36871760f7ec55dbd13cb1b85121a523102d11b2
Willy Scheibel <willy.scheibel@hpi.de>
2015-09-07 11:04:43 +0200

    Revert "workaround warning due to confusing implementation of setValue (still todo)"
    This reverts commit 5a998f63c405f9267c34b9882cec902dac066124.

--- source/reflectionzeug/include/reflectionzeug/property/PropertyUnsignedIntegral.hpp
+++ source/reflectionzeug/include/reflectionzeug/property/PropertyUnsignedIntegral.hpp
@@ -10,22 +10,6 @@
 namespace reflectionzeug
 {
 
-template <typename T>
-bool setValueULL(PropertyUnsignedIntegral<T> & p, const unsigned long long v);
-
-template <typename T>
-bool setValueULL<T>(PropertyUnsignedIntegral<T> & p, const unsigned long long v)
-{
-	p.setValue(static_cast<T>(v));
-	return true; // ToDo: this seems wrong! - why are there value setters returning void?
-};
-
-template <>
-REFLECTIONZEUG_API bool setValueULL<bool>(PropertyUnsignedIntegral<bool> & p, const unsigned long long v);
-
-template <>
-REFLECTIONZEUG_API bool setValueULL<const bool>(PropertyUnsignedIntegral<const bool> & p, const unsigned long long v);
-
 
 template <typename T>
 template <typename... Args>
@@ -42,15 +26,15 @@ PropertyUnsignedIntegral<T>::~PropertyUnsignedIntegral()
 template <typename T>
 unsigned long long PropertyUnsignedIntegral<T>::toULongLong() const
 {
-    return static_cast<unsigned long long>(value());
+    return static_cast<unsigned long long>(this->value());
 }
 
 template <typename T>
-bool PropertyUnsignedIntegral<T>::fromULongLong(unsigned long long v)
+bool PropertyUnsignedIntegral<T>::fromULongLong(unsigned long long value)
 {
-	return setValueULL(*this, v);
+    this->setValue(static_cast<T>(value));
+    return true;
 }
-
 
 template <typename T>
 void PropertyUnsignedIntegral<T>::accept(AbstractVisitor * visitor)
