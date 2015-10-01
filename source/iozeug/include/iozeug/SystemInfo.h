4c13c47df3684d02314e7bb887a50b4c4e322b01
Leander Neiß <leander.neiss@student.hpi.uni-potsdam.de>
2015-10-01 11:58:25 +0200

    Add static SystemInfo class for platform specific information like shared lib prefix/extension.

---
+++ source/iozeug/include/iozeug/SystemInfo.h
@@ -0,0 +1,42 @@
+
+#pragma once
+
+
+#include <string>
+
+#include <iozeug/iozeug_api.h>
+
+
+namespace iozeug
+{
+
+
+/**
+*  @brief
+*    Class to provide platform specific information like prefix or extension of shared libraries.
+*/
+class IOZEUG_API SystemInfo
+{
+public:
+
+    /**
+    *  @brief
+    *    Get platform specific path seperator.
+    */
+    static std::string pathSeperator();
+
+    /**
+    *  @brief
+    *    Get platform specific shared library prefix.
+    */
+    static std::string libPrefix();
+
+    /**
+    *  @brief
+    *    Get platform specific shared library extension.
+    */
+    static std::string libExtension();
+};
+
+
+} // namespace reflectionzeug
