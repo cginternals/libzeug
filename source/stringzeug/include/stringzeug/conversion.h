4d335e87e838f6c2bf4efe8b5e0ae0897b179fbe
Daniel Limberger <daniel.limberger@hpi.de>
2016-01-29 10:36:26 +0100

    add utf8 encoding to stringzeug

--- source/stringzeug/include/stringzeug/conversion.h
+++ source/stringzeug/include/stringzeug/conversion.h
@@ -54,7 +54,47 @@ STRINGZEUG_API std::string toString<unsigned char>(const unsigned char & value);
 //@}
 
 
+/**
+*  @brief
+*    The used encoding of a passed std::string.
+*
+*    This enum is mainly used to support the conversion of strings or a collections of characters to UTF-32, which is internally used by gloperate-text.
+*/
+enum class Encoding : unsigned int
+{
+    ANSI
+,   ASCII = Encoding::ANSI // for both ANSI and ASCII 1 byte in a std::string or char is used to encode it
+,   UTF8
+};
+
+STRINGZEUG_API std::u32string encode(const std::string & input, Encoding encoding);
+//STRINGZEUG_API std::u32string encode(const std::wstring & input, Encoding encoding);
+//STRINGZEUG_API std::u32string encode(const std::u16string & input, Encoding encoding);
+//STRINGZEUG_API std::u32string encode(const char * input, size_t size, Encoding encoding);
+//
+//STRINGZEUG_API void decode(const std::u32string & input, std::string & output, Encoding encoding);
+//STRINGZEUG_API void decode(const std::u32string & input, std::wstring & output, Encoding encoding);
+//STRINGZEUG_API void decode(const std::u32string & input, std::u16string & output, Encoding encoding);
+//STRINGZEUG_API void decode(const std::u32string & input, const char * & output, std::size_t size, Encoding encoding);
+
+
 } // namespace stringzeug
+
+
+namespace std
+{
+
+template<>
+struct hash<stringzeug::Encoding>
+{
+    std::hash<unsigned int>::result_type operator()(const stringzeug::Encoding & arg) const
+    {
+        std::hash<unsigned int> hasher;
+        return hasher(static_cast<unsigned int>(arg));
+    }
+};
+
+} // namespace std
 
 
 #include <stringzeug/conversion.hpp>
